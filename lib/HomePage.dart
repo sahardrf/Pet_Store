2abf9a1d6a2d89056dce2a26e7438c10e87fb40d
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-04-01 23:57:40 +0430

    NavigationBar Added to HomePage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,17 +1,87 @@
 import 'package:flutter/material.dart';
 
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
   @override
-  _HomePageState createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        title: 'Pet Store',
+        home: PetStoreHomePage(),
+);
+
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+ class PetStoreHomePage extends StatefulWidget {
+    const PetStoreHomePage({ Key? key }) : super(key: key);
+  
+    @override
+    _PetStoreHomePageState createState() => _PetStoreHomePageState();
+  }
+  
+  class _PetStoreHomePageState extends State<PetStoreHomePage> {
+  int _selectedIndex = 0;
+  static final List<Widget> _pages = <Widget>[
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("User Home Page"),
+        ],
+      ),
+    ),
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("List of Pets"),
+        ],
+      ),
+    ),
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("List of Stores"),
+        ],
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(
+      () {
+        _selectedIndex = index;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pet Store List...'),
+        title: const Text('Pet Store Home Page'),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pets),
+            label: "Pet",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_grocery_store),
+            label: "Store",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
