acd71f41f6417f837bf01279a47bfa195e9782b3
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-04-09 01:08:50 +0430

    cleaning the code

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,10 +1,5 @@
-import 'package:flutter/foundation.dart';
-
-import 'API.dart';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:pet_store/pet.dart';
-import 'package:pet_store/API.dart';
+import 'widgets/get_pet_list.dart';
 
 
 
@@ -28,13 +23,6 @@ class PetStoreHomePage extends StatefulWidget {
 }
 
 class _PetStoreHomePageState extends State<PetStoreHomePage> {
-  // late Future <List<dynamic>> pet_list;
-
-  // @override
-  // initState() {
-  //   super.initState();
-  //   pet_list = API.get_pets();
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -42,42 +30,7 @@ class _PetStoreHomePageState extends State<PetStoreHomePage> {
       appBar: AppBar(
         title: Text("Pets List"),
       ),
-      body: Center( 
-    child: FutureBuilder<List<dynamic>>(
-        future: API.get_pets(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: snapshot.data?.length,
-                itemBuilder: (context, index) {
-                  var id = snapshot.data![index]['id'].toString();
-                  var name = snapshot.data![index]['name'].toString();
-                  var category = snapshot.data![index]['category']['name'].toString();
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.indigo,
-                      ),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: ListTile(
-                      leading: Text(id),
-                      title: Text(name),
-                      subtitle: Text(category)
-                      ),
-                      // trailing: Text(Category),
-                    // ),
-                  );
-                },
-              ),
-            );
-          }
-          return CircularProgressIndicator();
-        },
-      ),
-    ),
+      body: Get_Pet_List(),
       );
   }
 }
