c1566ad8e5d8c95472c3aed50159783487d4cd92
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-04-09 01:23:31 +0430

    NavigationBar added to homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/get_pet_list.dart';
-
+import 'package:pet_store/widgets/get_pet_list.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -9,28 +8,82 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+        title: 'Pet Store',
         home: PetStoreHomePage(),
 );
 
   }
 }
 
-class PetStoreHomePage extends StatefulWidget {
-  const PetStoreHomePage({ Key? key }) : super(key: key);
+ class PetStoreHomePage extends StatefulWidget {
+    const PetStoreHomePage({ Key? key }) : super(key: key);
+  
+    @override
+    _PetStoreHomePageState createState() => _PetStoreHomePageState();
+  }
+  
+  class _PetStoreHomePageState extends State<PetStoreHomePage> {
+  int _selectedIndex = 0;
+  static final List<Widget> _pages = <Widget>[
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("User Home Page"),
+        ],
+      ),
+    ),
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Expanded(child: Get_Pet_List())
+        ],
+      ),
+    ),
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("List of Stores"),
+        ],
+      ),
+    ),
+  ];
 
-  @override
-  _PetStoreHomePageState createState() => _PetStoreHomePageState();
-}
-
-class _PetStoreHomePageState extends State<PetStoreHomePage> {
+  void _onItemTapped(int index) {
+    setState(
+      () {
+        _selectedIndex = index;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pets List"),
+        title: const Text('Pet Store Home Page'),
       ),
-      body: Get_Pet_List(),
-      );
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pets),
+            label: "Pet",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_grocery_store),
+            label: "Store",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
