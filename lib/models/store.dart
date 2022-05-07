f8c0b2a635f6336adf946903093b6360168799ce
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-05-08 00:42:42 +0430

    refactor: store as a model

---
+++ lib/models/store.dart
@@ -0,0 +1,23 @@
+class Store {
+  String approved = '';
+  String placed = '';
+  String delivered = '';
+
+  Store(String approved, String placed, String delivered) {
+    this.approved = approved;
+    this.placed = placed;
+    this.delivered = delivered;
+
+  }
+
+  Store.fromJson(Map json):
+        approved = json['approved'],
+        placed= json['placed'], 
+        delivered = json['delivered'];
+       
+
+  Map toJson() {
+    return {'approved': approved, 'placed': placed, 'delivered': delivered}; 
+    
+  }
+}
