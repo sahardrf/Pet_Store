7f89c3862d700e0298701f7d48e8b32353e3d9e5
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-05-28 21:45:49 +0430

    feat: login and hamburger menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:pet_store/Search.dart';
 import 'package:pet_store/widgets/get_pet_cards.dart';
 import 'package:pet_store/widgets/get_pet_listview.dart';
 import 'package:pet_store/widgets/get_store_data.dart';
 import 'package:double_back_to_close/double_back_to_close.dart';
+import 'package:pet_store/widgets/login.dart';
 
-void main() => runApp(const HomePage());
+void main() => runApp(HomePage());
  
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  // bool? LoggedIn;
+ const HomePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,8 +31,10 @@ class PetStoreHomePage extends StatefulWidget {
   // const PetStoreHomePage({ Key? key }) : super(key: key);
   // final String url = '';
   final String? selectedURL;
+  final bool? LoggedIn;
   PetStoreHomePage({
     this.selectedURL,
+    this.LoggedIn = false
   });
 
   @override
@@ -52,7 +58,7 @@ class _PetStoreHomePageState extends State<PetStoreHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigo,
-        title: Text('Home Page'),
+        title: Text('Pets'),
         actions: <Widget>[
           IconButton(
             icon: Icon(
@@ -104,17 +110,88 @@ class _PetStoreHomePageState extends State<PetStoreHomePage> {
           ),
       ),
     ), 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        backgroundColor: Colors.indigo,
-        child: new IconTheme(
-    data: new IconThemeData(
-        color: Colors.white), 
-    child: new Icon(Icons.add),
-),
+    drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+              ),
+              child: Center(child: ListTile(
+                leading: Icon(Icons.portrait_rounded, size:80, color: Colors.white,),
+                title: Text('Pet Store', style: TextStyle(fontWeight:FontWeight.bold, fontSize: 20, color: Colors.white),
+                ),
+                ),
+                ),  //USER Profile if log in
+            ),
+            ListTile(
+              leading: (widget.LoggedIn == true)? Icon(Icons.logout, color: Colors.black,):Icon(Icons.login, color: Colors.black,),
+              title: (widget.LoggedIn == true)? Text('Log out'): Text('Login'), //change to logout if logged in
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                if (widget.LoggedIn == false){
+                    Navigator.pushAndRemoveUntil(context,
+                                                MaterialPageRoute(
+                                                  builder: (BuildContext context) => const login(),
+                                                ),
+                                                (route) => false,
+                                              );
+              }
+               else if(widget.LoggedIn == true){
+                    Navigator.pushAndRemoveUntil(context,
+                                                MaterialPageRoute(
+                                                  builder: (BuildContext context) => PetStoreHomePage(LoggedIn: false,),
+                                                ),
+                                                (route) => false,
+                                              );
+
+                }
+              } // OnTap
+            ),
+            ExpansionTile(
+            leading: const Icon(Icons.pets, color: Colors.black),
+            title: const Text('Pet'),
+            children: <Widget>[
+                                // ListTile(leading: Icon(Icons.search),title:Text("Find Pet")),
+                                TextButton(child: const ListTile(leading: Icon(Icons.add),title:Text("Add a new Pet")), onPressed: () => Navigator.pop(context),),
+                                // ListTile(leading: Icon(Icons.add),title:Text("Add a new Pet"))
+                              ],
+              // onTap: () {
+              //   // Update the state of the app
+              //   // ...
+              //   // Then close the drawer
+              //   Navigator.pop(context);
+              // },
+            ),
+            ExpansionTile(
+            leading: const Icon(Icons.local_grocery_store, color: Colors.black),
+            title: const Text('Store'),
+            children: <Widget>[
+                                TextButton(child: const ListTile(leading: Icon(Icons.attach_money),title:Text("Place Order")), onPressed: () => Navigator.pop(context),),
+                                TextButton(child: const ListTile(leading: Icon(Icons.search),title:Text("Find an Order")), onPressed: () => Navigator.pop(context),),
+                                ],
+            )  
+          ],
+        ),
       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           // Add your onPressed code here!
+//         },
+//         backgroundColor: Colors.indigo,
+//         child: new IconTheme(
+//     data: new IconThemeData(
+//         color: Colors.white), 
+//     child: new Icon(Icons.add),
+// ),
+//       ),
     );
     
   }
