c4c6870454f9d83e830151b96cbdeb2389485932
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-07-14 20:06:45 +0430

    feat: creating a pet

--- lib/AddPet.dart
+++ lib/AddPet.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:pet_store/main.dart';
 import 'package:pet_store/widgets/tag_preview.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:math';
+import 'package:http/http.dart' as http;
+
 
 class add_pet extends StatefulWidget {
   const add_pet({Key? key}) : super(key: key);
@@ -9,9 +17,25 @@ class add_pet extends StatefulWidget {
 }
 
 class _add_petState extends State<add_pet> {
+
+  
+  final _picker = ImagePicker();
+  Future<void> _openImagePicker() async {
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        final bytes = File(pickedImage.path).readAsBytesSync();
+        base64Image =  "data:image/png;base64,"+base64Encode(bytes);
+      });
+    }
+  }
+  var random = new Random();
+  String? base64Image;
   TextEditingController nameController = TextEditingController();
   TextEditingController categoryController = TextEditingController();
   TextEditingController tagController = TextEditingController();
+  late File imageFile;
   List<tagPreview> dynamicList = [];
   List<String> tags = [];
   void initState() {
@@ -28,10 +52,11 @@ class _add_petState extends State<add_pet> {
   }
 
   String? dropdownvalue;
+
   var items = [
-    'Available',
-    'Pending',
-    'Sold',
+    'available',
+    'pending',
+    'sold',
   ];
 
   @override
@@ -40,6 +65,16 @@ class _add_petState extends State<add_pet> {
       appBar: AppBar(
         backgroundColor: Colors.indigo,
         title: const Text('Add a new pet'),
+        leading: GestureDetector(
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          onTap: () {
+            // Navigator.pop(context);
+            Navigator.push(context, MaterialPageRoute(builder: (_) => const HomePage()));
+          },
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -61,7 +96,7 @@ class _add_petState extends State<add_pet> {
                       controller: nameController,
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: 'Name',
+                        labelText: "Enter your pet's name",
                       ),
                     ),
                     DropdownButton(
@@ -106,7 +141,7 @@ class _add_petState extends State<add_pet> {
                       controller: categoryController,
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: "Specify your pets' category",
+                        labelText: "Specify your pet's category",
                       ),
                     ),
                   ],
@@ -135,26 +170,26 @@ class _add_petState extends State<add_pet> {
                     Flexible(
                       fit: FlexFit.loose,
                       child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: tags.length,
-                          itemBuilder: (_, index) {
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 5.0, vertical: 3.0),
-                              child: GestureDetector(child: tagPreview(tags[index]),
-                              onTap: (){
-                                tags.removeAt(index);
-                              }
-                              ),
-                            );
-                          },
-                         ),
+                        shrinkWrap: true,
+                        itemCount: tags.length,
+                        itemBuilder: (_, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 5.0, vertical: 3.0),
+                            child: GestureDetector(
+                                child: tagPreview(tags[index]),
+                                onTap: () {
+                                  tags.removeAt(index);
+                                }),
+                          );
+                        },
+                      ),
                     ),
                     TextField(
                       controller: tagController,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: "Specify your pets' tags",
+                        labelText: "Specify your pet's tags",
                         suffixIcon: IconButton(
                           icon: const Icon(
                             Icons.add_circle_outline_sharp,
@@ -180,11 +215,41 @@ class _add_petState extends State<add_pet> {
                   borderSide: const BorderSide(color: Colors.white)),
             ),
             Card(
-              child: const ListTile(
-                title: Text(
-                  "Images",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+              child: Column(
+                children: [
+                  const ListTile(
+                    title: Text(
+                      "Images",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 100),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            const Color.fromARGB(255, 129, 128, 128)),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(horizontal: 20)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            side: const BorderSide(
+                              color: Color.fromARGB(255, 129, 128, 128),
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        'Select Images',
+                        style: TextStyle(fontSize: 15.0, color: Colors.white),
+                      ),
+                      onPressed:_openImagePicker
+                    ),
+                  ),
+                ],
               ),
               elevation: 8,
               shadowColor: Colors.grey.shade300,
@@ -214,9 +279,44 @@ class _add_petState extends State<add_pet> {
                   'Add Pet',
                   style: TextStyle(fontSize: 17.0, color: Colors.white),
                 ),
-                onPressed: () {
+                onPressed: () async {
                   print('Pressed');
-                },
+                  // handle categories and tangs as list of json objects
+                  Map data = {
+                            "id": random.nextInt(10000),    ///random it integer
+                            "name": nameController.text,
+                            "category": {
+                              "id": set_category_id(categoryController.text) ,
+                              "name": categoryController.text,
+                            },
+                            "photoUrls": [
+                              base64Image
+                            ],
+                            "tags": set_tags(tags),
+                            "status": dropdownvalue
+                          };
+                  var body = json.encode(data);
+                  var response = await http.post(
+                      Uri.parse("https://api.training.testifi.io/api/v3/pet"),
+                      headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                      },
+                      body: body);
+                      CircularProgressIndicator();
+                      if (response.statusCode == 200) {
+                        print('success');
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Pet is successfully created."),
+                      ));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage()));
+                      } else {
+                        print('error');
+                      }
+                  }
               ),
             ),
           ],
@@ -224,5 +324,36 @@ class _add_petState extends State<add_pet> {
       ),
     );
   }
+
+int set_category_id(String category_name){
+  int category_id;
+  if (category_name == 'Cat' || category_name == 'cat') {
+    category_id = 0;
+  } else if (category_name == 'Dog' || category_name == 'dog') {
+    category_id = 1;
+  } else if (category_name == 'Bunny' || category_name == 'bunny') {
+    category_id = 2;
+  } else if (category_name == 'Fish' || category_name == 'fish') {
+    category_id = 3;
+  } else if (category_name == 'Bird' || category_name == 'bird') {
+    category_id = 4;
+  } else {
+    category_id = 5;
+  }
+  return category_id;
 }
 
+List set_tags(List tags){
+  List<dynamic> tags_json = [];
+  for (int i = 0; i < tags.length; i++) {
+    var temp = {
+      "id": i,
+      "tag": tags[i]
+    };
+    tags_json.add(temp);
+  }
+  print(tags_json);
+  return tags_json;
+}
+
+}
