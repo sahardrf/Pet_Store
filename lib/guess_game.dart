09da27a2fa101c54a15bd554c93a696b835dff7a
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-07-25 04:06:11 +0430

    feat: guess game functionality completed

--- lib/guess_game.dart
+++ lib/guess_game.dart
@@ -1,8 +1,10 @@
+import 'package:double_back_to_close/toast.dart';
 import 'package:flutter/material.dart';
 import 'webservice/API.dart';
 import 'main.dart';
 import 'dart:math';
 import 'models/pet.dart';
+import 'utils/utils.dart';
 
 Random random = new Random();
 
@@ -17,6 +19,7 @@ class _Guess_GameState extends State<Guess_Game> {
   String? dropdownvalue;
 
   var items = ['Bunny', 'Fish', 'Hedgehog', 'Kitty', 'Puppy'];
+  var dropDownIsSelected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -50,9 +53,10 @@ class _Guess_GameState extends State<Guess_Game> {
                     fontSize: 20,
                     color: Color.fromARGB(255, 40, 48, 95))),
             const Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(1.0),
               child: Text("Guess which animal will we generate randomly"),
             ),
+            // Text("Your Guess: " + dropdownvalue.toString()),
             DropdownButton(
               hint: const Text("My Guess"),
               value: dropdownvalue,
@@ -66,32 +70,154 @@ class _Guess_GameState extends State<Guess_Game> {
               onChanged: (String? newValue) {
                 setState(() {
                   dropdownvalue = newValue!;
+                  dropDownIsSelected = true;
                 });
               },
             ),
-
+            //get pets and show one of them randomly
+            const Padding(
+                padding: EdgeInsets.all(1.0),
+                child: Text("                        ")),
+            (dropDownIsSelected == true)
+                ? FutureBuilder<List<Pet>>(
+                    future: API.get_pets(randomly_select_URL()),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        if (dropDownIsSelected == true) {
+                          var number_of_parameters = snapshot.data!.length;
+                          var random_pet = random.nextInt(number_of_parameters);
+                          return Expanded(
+                            child: PageView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: number_of_parameters,
+                              onPageChanged: (int index) {
+                                setState(() {
+                                  dropdownvalue = null;
+                                  dropDownIsSelected = false;
+                                });
+                              },
+                              itemBuilder: (context, index) {
+                                Pet pet = snapshot.data![random_pet];
+                                var id = pet.id.toString();
+                                var name = pet.name.toString();
+                                var category = pet.category.toString();
+                                var status = pet.status.toString();
+                                var photoURL = pet.photoUrls.toString();
+                                return Card(
+                                  child: Container(
+                                    width: 100,
+                                    height: 150,
+                                    decoration: (photoURL.length >= 50)
+                                        ? BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(15),
+                                            image: DecorationImage(
+                                                opacity: 0.6,
+                                                image: image(photoURL).image,
+                                                fit: BoxFit.scaleDown),
+                                          )
+                                        : BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(15),
+                                            image: const DecorationImage(
+                                                opacity: 0.6,
+                                                image: NetworkImage(
+                                                    "https://cdn-cziplee-estore.azureedge.net//cache/no_image_uploaded-253x190.png"),
+                                                fit: BoxFit.scaleDown),
+                                          ),
+                                    child: Container(
+                                      padding: EdgeInsets.all(5),
+                                      child: Column(children: [
+                                        Text("Your Guess:  " +
+                                            dropdownvalue.toString()),
+                                        Text("Actual Pet:  " +
+                                            category.toString()),
+                                        (category == dropdownvalue)
+                                            ? const Text("You Win!",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.green))
+                                            : const Text("Maybe Next Time :(",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.red)),
+                                        const Text('                       '),
+                                        (id == null)
+                                            ? const Text("Null")
+                                            : Text(id),
+                                        (name == null)
+                                            ? const Text("Null")
+                                            : Text(
+                                                name,
+                                                style: const TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            (category == null)
+                                                ? const Text("Null")
+                                                : Text(category),
+                                            const Text(" | "),
+                                            (status.isEmpty)
+                                                ? const Text("Null")
+                                                : Text(
+                                                    status,
+                                                    style: TextStyle(
+                                                        color: (status ==
+                                                                'available')
+                                                            ? Colors.green
+                                                            : (status ==
+                                                                    'pending')
+                                                                ? const Color
+                                                                        .fromARGB(
+                                                                    255,
+                                                                    255,
+                                                                    174,
+                                                                    0)
+                                                                : Colors.red),
+                                                  ),
+                                          ],
+                                        ),
+                                      ]),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        } 
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+                      return const CircularProgressIndicator();
+                    },
+                  )
+                //check if category and dropdownvalue are equal or not
+                : const Text(
+                    "Please select your guess",
+                    style: TextStyle(fontSize: 17, color: Colors.indigo),
+                  ),
+              (dropDownIsSelected==true)? ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.indigo,
+                    
+                  ),
+                  child: const Text('Remove', style: TextStyle(color: Colors.white)),
+                  onPressed: () {
+                    setState(() {
+                                          dropDownIsSelected = false;
+                    dropdownvalue = null;
+                    });
+                    Toast.show("Play Again!", context);
+                  }):const Text(""),
           ],
         ),
       ),
     );
-  }
-
-  randomly_select_URL() {
-    var selectedURLID = random.nextInt(150);
-
-    String url;
-    if (selectedURLID <= 50) {
-      url =
-          'https://api.training.testifi.io/api/v3/pet/findByStatus?status=available';
-      return url;
-    } else if (selectedURLID <= 100 && selectedURLID > 50) {
-      url =
-          'https://api.training.testifi.io/api/v3/pet/findByStatus?status=pending';
-      return url;
-    } else if (selectedURLID <= 150 && selectedURLID > 100) {
-      url =
-          'https://api.training.testifi.io/api/v3/pet/findByStatus?status=sold';
-      return url;
-    }
   }
 }
