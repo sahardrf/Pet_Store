7c7d8f85b20ad1fa8cfa8e480d12bdc6e9524099
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-08-14 22:28:27 +0430

    feat: tag retrieve preview for pet update

---
+++ lib/widgets/tag_retrieve_preview.dart
@@ -0,0 +1,58 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:pet_store/models/tag.dart';
+
+class tagRetrievePreview extends StatelessWidget {
+  var tag;
+
+  tagRetrievePreview(this.tag, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Card(
+          color: const Color.fromARGB(255, 224, 223, 223),
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(
+              color: Color.fromARGB(255, 83, 83, 83),
+            ),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Row(
+              children: [
+                Text(
+                  tag,
+                  style: TextStyle(color: Colors.grey.shade800),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      print('Delete tag button tapped');
+                    },
+                    child: const CircleAvatar(
+                        radius: 8,
+                        backgroundColor: Color.fromARGB(255, 117, 112, 112),
+                        child: Icon(
+                          Icons.close,
+                          size: 13,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
