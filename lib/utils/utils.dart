269e3dcc277d28415aaeb73c5050ec4fba5f5893
Sahar Darafsh <sahar,drfsh@gmail.com>
2022-08-14 22:28:59 +0430

    refactor: set category and tag functions

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -41,3 +41,44 @@ String randomly_select_URL() {
   }
   return url;
 }
+
+int set_category_id(String category_name) {
+  int category_id;
+  if (category_name == 'Cat' || category_name == 'cat') {
+    category_id = 0;
+  } else if (category_name == 'Dog' || category_name == 'dog') {
+    category_id = 1;
+  } else if (category_name == 'Bunny' ||
+      category_name == 'bunny' ||
+      category_name == 'Rabbit' ||
+      category_name == 'rabbit') {
+    category_id = 2;
+  } else if (category_name == 'Fish' || category_name == 'fish') {
+    category_id = 3;
+  } else if (category_name == 'Hedgehog' || category_name == 'hedgehog') {
+    category_id = 4;
+  } else {
+    category_id = 5;
+  }
+  return category_id;
+}
+
+set_tags(List tags) {
+  List<dynamic> tags_json = [];
+  for (int i = 0; i < tags.length; i++) {
+    var temp = {"id": i, "name": tags[i]};
+    tags_json.add(temp);
+  }
+  print(tags_json);
+  return tags_json;
+}
+
+// show_previous_tags(Map tags){
+//   List<dynamic> tags_json = [];
+//   for (int i = 0; i < tags.length; i++) {
+//     var temp = {"id": i, "name": tags[i]};
+//     tags_json.add(temp);
+//   }
+//   print(tags_json);
+//   return tags_json;
+// }
